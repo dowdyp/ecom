08071785e1967676c25d8018098ca12150e07d45
lelkek <monorail65@gmail.com>
2017-10-06 11:26:35 -0400

    maybe?

--- src/js/main.js
+++ src/js/main.js
@@ -10,25 +10,32 @@ $(document).ready(function() {
 var ecom = angular.module('ecom', []);
 
 ecom.controller('ecomCtrl', ['$scope', function($scope) {
-	var rec_products = [
-	{
-		keyword: 'Hoodie',
-		desc: 'hoodie',
-		pid: 1,
-		price: 29.99
-	},
-	{
-		keyword: 'T-Shirt',
-		desc: 't shirt'
-		pid: 2,
-		price: 19.99
-	},
-	{
-		keyword: 'Pants',
-		desc: 'pants',
-		pid: 3,
-		price: 24.99
-	}
+	
+	// In production rec_products will = [] and will be filled by an $http.get call to the host server
+		var root_url = 'localhost/';
+
+		$scope.rec_products = [
+		{
+			thumb_url: './data/images/hoodie.jpg',
+			keyword: 'Hoodie',
+			desc: 'hoodie',
+			pid: 1,
+			price: 29.99
+		},
+		{
+			thumb_url: './data/images/tshirt.jpg',
+			keyword: 'T-Shirt',
+			desc: 't shirt',
+			pid: 2,
+			price: 19.99
+		},
+		{
+			thumb_url: './data/images/pants.jpg',
+			keyword: 'Pants',
+			desc: 'pants',
+			pid: 3,
+			price: 24.99
+		}
 	];
 
 }]);
